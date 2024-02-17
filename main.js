adf7d42e6f98b3dd3645fd66438f46118881f280
ARANO8 <alanarnez51@gmail.com>
2024-02-17 18:24:17 -0400

    audio

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 onload = () => {
+    PlayAudio();
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
       clearTimeout(c);
     }, 1000);
-  };
+};
+function PlayAudio() {
+  var audio = document.getElementById("audio");
+  audio.play();
+}
